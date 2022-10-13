ef233d313bf8661b122bef95129a203a4964a285
Karan Singh Sandhu <karan.s.01@hotmail.com>
2022-10-13 13:22:37 +0200

    addedgraphQL files

--- project3/src/index.tsx
+++ project3/src/index.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloServer } from "apollo-server";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 1
+import { schema } from "./schema";
+export const server = new ApolloServer({
+    schema,
+});
+
+const port = 3000;
+// 2
+server.listen({port}).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
