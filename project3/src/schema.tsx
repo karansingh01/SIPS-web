44abcc3a21b7d92576a70bd007387dc9a3d62b4a
Karan Singh Sandhu <karan.s.01@hotmail.com>
2022-10-16 16:33:01 +0200

    added example of sips

--- project3/src/schema.tsx
+++
@@ -1,9 +0,0 @@
-import { makeExecutableSchema } from "graphql-tools";
-import { ServiceTypeDefs } from "./service/serviceSchema";
-import serviceResolvers from "./service/serviceResolver";
-import ServiceResolvers from "./service/serviceResolver";
-
-export const schema = makeExecutableSchema({
-  typeDefs: ServiceTypeDefs,
-  resolvers: ServiceResolvers,
-});
