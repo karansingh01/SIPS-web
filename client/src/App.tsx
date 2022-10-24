ec97919e53f74d5efb620526ec1b28a19785e3e3
Karan Singh Sandhu <karan.s.01@hotmail.com>
2022-10-24 21:32:23 +0200

    pushing to share code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,105 @@
 
 import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, ApolloClient, InMemoryCache } from '@apollo/client';
+/* import { JsonApiLink } from "apollo-link-json-api";
 
 
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
+const jsonApiLink = new JsonApiLink({
+  uri: 'http://jsonapiplayground.reyesoft.com/v2/',
+});
+// Configure the ApolloClient with the default cache and JsonApiLink
+const client = new ApolloClient({
+  uri: jsonApiLink,
+  cache: new InMemoryCache(),
+});
+// A simple query to retrieve data about the first author
+const query = gql`
+  query firstAuthor {
+    author @jsonapi(path: "authors/1") {
       name
-      description
-      photo
+    }
+  }
+  `; 
+  // Invoke the query and log the person's name
+client.query({ query }).then(response => {
+  console.log(response.data.name);
+});
+*/
+
+
+const GET_DRINKS = gql`
+query getDrinks{
+    drinks{
+      idDrink
+      strDrink
+    }
+    }
+`; 
+const GET_DRINKS5 = gql`
+query cocktail($id: ID!) {
+  cocktail(id: $id) {
+    id
+    likes
+    glassType
+    instructions
+    ingredients {
+      name
+      quantity
+    }
+    liked
+    bookmarked
+  }
+}
+`; 
+
+
+const GET_GEN_3 = gql`
+  query g3 {
+    pokemon_v2_pokemonspecies{
+      name
+      id
     }
   }
 `;
 
-export default function App() {
-  return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <h2>Console.logger fra en nettside ved bruk av apollo</h2>
-      <br />
-{/*       <DisplayLocations />
- */}
-    </div>
-  );
+const GET_DRINKS2 = gql`
+query cocktails($ingredient: String) {
+  cocktails(ingredient: $ingredient) {
+    id
+    name
+    imageURL
+  }
 }
+`;
+const GET_DRINKS3 = gql`
+query cocktail($id: ID!) {
+  cocktail(id: $id) {
+    id
+    likes
+    glassType
+    instructions
+  }
+}
+`; 
 
+export default function App() {
+    const {loading, error, data} =useQuery(GET_DRINKS);
+    console.log(data);
+    if (data){
+      return (
+          <div>
+        <p>data</p> 
+        <h2>My first Apollo app 🚀</h2>
+        <h2>Console.logger fra en nettside ved bruk av apollo</h2>
+        <br />
+      </div>
+    );
+      }
+      else{
+        return (
+          <div>
+            <p>fuck</p>
+          </div>
+        )
+      }
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, ApolloLink } from '@apollo/client';
+import { JsonApiLink } from "apollo-link-json-api";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-
 const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',   // her må iv hente API?
+  uri: 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink',
+  cache: new InMemoryCache()
+});
+/* const client = new ApolloClient({
+  uri: 'https://beta.pokeapi.co/graphql/v1beta',
+  cache: new InMemoryCache()
+}); */
+
+/* const client = new ApolloClient({
+  uri: 'https://localhost4000',   // her må iv hente API?
   cache: new InMemoryCache(),
 });
-
-client
+ */
+/* client
   .query({
     query: gql`
       query GetLocations {
@@ -27,12 +36,10 @@ client
       }
     `,
   })
-  .then((result) => console.log(result));
+  .then((result) => console.log(result)); */
 
   root.render(
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>,
   );
-
-
