34b0eb3fc8a241034f5868a8d0314a4be7f56c6d
Carolina Gunnesdal <carolinagunnesdal@dhcp-10-22-8-201.wlan.ntnu.no>
2022-10-24 10:57:37 +0200

    Created first design of the navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,96 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { FaHeart } from "react-icons/fa";
+
+const TITLE: string = "SIPS";
+
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("transparent", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Box color={"beige"} fontSize={"80px"} margin-left={"3%"}>
+            {TITLE}
+          </Box>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button color={"lightpink"}>
+                <FaHeart />
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar
+                    size={"sm"}
+                    src={
+                      "https://upload.wikimedia.org/wikipedia/commons/2/24/Missing_avatar.svg"
+                    }
+                  />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={"2xl"}
+                      src={
+                        "https://upload.wikimedia.org/wikipedia/commons/2/24/Missing_avatar.svg"
+                      }
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
--- client/src/pages/FavoritesPage.tsx
+++ client/src/pages/FavoritesPage.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { SimpleGrid, Flex, Text } from "@chakra-ui/react";
 import CocktailCard from "../components/CocktailCard";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import "./Pages.css";
 
 const FavoritesPage = () => {
   return (
-    <Flex flexDirection="column">
-      <Text>Hei carolina, here are your favz</Text>
-    </Flex>
+    <div className="lol">
+      <Flex flexDirection="column">
+        <Navbar />
+        <Text>Hei carolina, here are your favz</Text>
+      </Flex>
+    </div>
   );
 };
 
