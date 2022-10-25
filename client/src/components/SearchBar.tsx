6c0775504184e5d220c21662205753ed41f4f7a9
sunnivabjerke <69514415+sunnivabjerke@users.noreply.github.com>
2022-10-25 15:23:48 +0200

    fixed search logic

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,20 +1,38 @@
-import { Box, Heading, Text, Stack, Container, Button, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Stack,
+  Container,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 
-export default function SearchBar({q, searchItems}:{q : string, searchItems : Function}) {
-    
-    return(
-        <Stack spacing={4}>
-            <InputGroup>
-                <InputLeftElement pointerEvents='none' children={<FaSearch color='beige' />} />
-                <Input 
-                    type={'search'} 
-                    placeholder='What drink are you looking for?'
-                    color={'beige'}
-                    value = {q}
-                    onChange={(e) => searchItems(e.target.value)}
-                    />    
-            </InputGroup>
-        </Stack>
-    )
+export default function SearchBar({
+  q,
+  setQuery,
+}: {
+  q: string;
+  setQuery: Function;
+}) {
+  return (
+    <Stack spacing={4}>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<FaSearch color="beige" />}
+        />
+        <Input
+          type={"search"}
+          placeholder="What drink are you looking for?"
+          color={"beige"}
+          value={q}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </InputGroup>
+    </Stack>
+  );
 }
--- client/src/pages/AllCocktailsPage.tsx
+++ client/src/pages/AllCocktailsPage.tsx
@@ -6,34 +6,45 @@ import { useState, useEffect } from "react";
 import dummyCocktails from "../DummyData";
 import CocktailCardsDisplay from "../components/CocktailCardsDisplay";
 
-const AllCocktailsPage = () => {
-  /* For søk. Denne funker ikke heeelt som jeg vil atm */
-  const [query, setQuery] = useState("");
-  const [cocktails, setCocktails] = useState(dummyCocktails);
-  const [filteredResults, setFilteredResults] = useState(dummyCocktails);
+/**
+ * search filter method
+ * @param cocktails all cocktails
+ * @param query string entered in search field
+ * @returns all cocktails containing the search param
+ */
+const filterCocktails = (
+  cocktails: {
+    id: number;
+    name: string;
+    description: string;
+    favorite: boolean;
+    image: string;
+  }[],
+  query: string
+) => {
+  if (query === "") {
+    return cocktails;
+  } else {
+    const filtered = cocktails.filter((cocktail) => {
+      return Object.values(cocktail.name)
+        .join("")
+        .toLowerCase()
+        .includes(query.toLowerCase());
+    });
+    return filtered;
+  }
+};
 
-  const searchItems = (searchValue: string) => {
-    setQuery(searchValue);
-    console.log(searchValue);
-    if (query !== "") {
-      const filteredData = cocktails.filter((cocktail) => {
-        return Object.values(cocktail.name)
-          .join("")
-          .toLowerCase()
-          .includes(query.toLowerCase());
-      });
-      setFilteredResults(filteredData);
-    } else {
-      setFilteredResults(cocktails);
-    }
-  };
+const AllCocktailsPage = () => {
+  const [query, setQuery] = useState("");
+  const cocktails = dummyCocktails;
+  const filteredCocktails = filterCocktails(cocktails, query);
 
   return (
     <Flex flexDirection="column">
       <Header />
       <Grid
-        /* transform="translate(0%, -15%)" */
-        transform="translate(0px, -150px)" //piksler for å unngå at designet "hopper"
+        transform="translate(0px, -150px)"
         margin={"30px"}
         rounded={20}
         templateRows="repeat(1fr)"
@@ -42,10 +53,10 @@ const AllCocktailsPage = () => {
         mt={5}
       >
         <GridItem colSpan={4}>
-          <SearchBar q={query} searchItems={searchItems} />
+          <SearchBar q={query} setQuery={setQuery} />
         </GridItem>
         <GridItem colSpan={4}>
-          <CocktailCardsDisplay cocktails={filteredResults} />
+          <CocktailCardsDisplay cocktails={filteredCocktails} />
         </GridItem>
       </Grid>
     </Flex>
