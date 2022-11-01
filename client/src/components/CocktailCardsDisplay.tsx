55d098b31b622b489a22ca17bcb91c08dfe51a40
torsteinko <torstein@knett.no>
2022-11-01 13:43:16 +0100

    #37 Modals now fully functioning.

--- client/src/components/CocktailCardsDisplay.tsx
+++ client/src/components/CocktailCardsDisplay.tsx
@@ -11,6 +11,18 @@ export default function CocktailCardsDisplay({
     idDrink: string;
     strDrink: string;
     strDrinkThumb: string;
+    strGlass: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
+    strIngredient10: string;
+    strInstructions: string;
   }[];
 }) {
   const [sortedDown, setSortedDown] = useState(true);
