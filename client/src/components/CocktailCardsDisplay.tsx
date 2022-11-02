dd91059cd9bbb925c540c499c04fd77ddeeb59f4
sunnivabjerke <69514415+sunnivabjerke@users.noreply.github.com>
2022-11-02 21:15:30 +0100

    only sorts cocktails currently in ui

--- client/src/components/CocktailCardsDisplay.tsx
+++ client/src/components/CocktailCardsDisplay.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text, Stack } from "@chakra-ui/react";
 import React from "react";
 import { useEffect, useState } from "react";
 import { FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa";
@@ -38,30 +38,75 @@ export default function CocktailCardsDisplay({
   const [sortedDown, setSortedDown] = useState(true);
 
   const toggleSort = () => {
-    cocktails.reverse();
+    /*  cocktails.reverse(); */
     setSortedDown(!sortedDown);
+    sorted(cocktails);
   };
-  /* For søk. Denne funker ikke helt som jeg vil atm */
-  /*       const [query, setQuery] = useState('')
-      const [cocktails, setCocktails] = useState(dummyCocktails);
-      const [filteredResults, setFilteredResults] = useState(dummyCocktails);
 
-       const searchItems = (searchValue : string) => {
-         setQuery(searchValue);
-         console.log(searchValue);
-         if (query !== ''){
-         const filteredData = cocktails.filter((cocktail) => {
-            return Object.values(cocktail).join('').toLowerCase().includes(query.toLowerCase())
-        })
-         setFilteredResults(filteredData)
-        } else {
-         setFilteredResults(cocktails)
-        }
-       }; */
+  const sorted = (
+    cocktails: {
+      idDrink: string;
+      strDrink: string;
+      strDrinkThumb: string;
+      strGlass: string;
+      strIngredient1: string;
+      strIngredient2: string;
+      strIngredient3: string;
+      strIngredient4: string;
+      strIngredient5: string;
+      strIngredient6: string;
+      strIngredient7: string;
+      strIngredient8: string;
+      strIngredient9: string;
+      strIngredient10: string;
+      strInstructions: string;
+    }[]
+  ) => {
+    let sortedCocktails: {
+      idDrink: string;
+      strDrink: string;
+      strDrinkThumb: string;
+      strGlass: string;
+      strIngredient1: string;
+      strIngredient2: string;
+      strIngredient3: string;
+      strIngredient4: string;
+      strIngredient5: string;
+      strIngredient6: string;
+      strIngredient7: string;
+      strIngredient8: string;
+      strIngredient9: string;
+      strIngredient10: string;
+      strInstructions: string;
+    }[] = [];
+    cocktails.forEach((cocktail) =>
+      sortedCocktails.push({
+        idDrink: cocktail.idDrink,
+        strDrink: cocktail.strDrink,
+        strDrinkThumb: cocktail.strDrinkThumb,
+        strGlass: cocktail.strGlass,
+        strIngredient1: cocktail.strIngredient1,
+        strIngredient2: cocktail.strIngredient2,
+        strIngredient3: cocktail.strIngredient3,
+        strIngredient4: cocktail.strIngredient4,
+        strIngredient5: cocktail.strIngredient5,
+        strIngredient6: cocktail.strIngredient6,
+        strIngredient7: cocktail.strIngredient7,
+        strIngredient8: cocktail.strIngredient8,
+        strIngredient9: cocktail.strIngredient9,
+        strIngredient10: cocktail.strIngredient10,
+        strInstructions: cocktail.strInstructions,
+      })
+    );
+    console.log(sortedCocktails);
+    sortedCocktails.sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1));
+    console.log(sortedCocktails);
+    return sortedCocktails;
+  };
 
   return (
     <div id={"cocktailCardsDisplay"}>
-      {/* <Stack marginLeft={20} marginTop={20}> */}
+      {/*  <Stack marginLeft={20} marginTop={20}> */}
       {sortedDown ? (
         <FaSortAlphaDown
           onClick={toggleSort}
@@ -81,7 +126,7 @@ export default function CocktailCardsDisplay({
           }}
         />
       )}
-      {/*  </Stack> */}
+      {/* </Stack> */}
       <SimpleGrid
         columns={[1, 2, 3]}
         minChildWidth={"330px"}
@@ -92,13 +137,13 @@ export default function CocktailCardsDisplay({
       >
         {cocktails.length > 0 ? (
           <>
-            {cocktails[0] ? (
-              cocktails.map((cocktail, index) => (
-                <CocktailCard key={index} cocktail={cocktail} />
-              ))
-            ) : (
-              <Text>There are no cocktails available</Text>
-            )}
+            {sortedDown
+              ? cocktails.map((cocktail, index) => (
+                  <CocktailCard key={index} cocktail={cocktail} />
+                ))
+              : sorted(cocktails).map((cocktail, index) => (
+                  <CocktailCard key={index} cocktail={cocktail} />
+                ))}
           </>
         ) : (
           <Text>There are no cocktails available</Text>
