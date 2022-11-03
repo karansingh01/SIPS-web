ac8d514b0126528caeab577b9f034c3355ee5693
Karan Singh Sandhu <karan.s.01@hotmail.com>
2022-11-03 17:43:33 +0100

    merged with main

--- client/src/components/CocktailCardsDisplay.tsx
+++ client/src/components/CocktailCardsDisplay.tsx
@@ -1,7 +1,5 @@
 import { SimpleGrid, Text, Stack } from "@chakra-ui/react";
 import React from "react";
-import { useEffect, useState } from "react";
-import { FaSortAlphaDown, FaSortAlphaUp } from "react-icons/fa";
 import CocktailCard from "./CocktailCard";
 
 export default function CocktailCardsDisplay({
@@ -35,98 +33,8 @@ export default function CocktailCardsDisplay({
     strMeasure10: string;
   }[];
 }) {
-  const [sortedDown, setSortedDown] = useState(true);
-
-  const toggleSort = () => {
-    /*  cocktails.reverse(); */
-    setSortedDown(!sortedDown);
-    sorted(cocktails);
-  };
-
-  const sorted = (
-    cocktails: {
-      idDrink: string;
-      strDrink: string;
-      strDrinkThumb: string;
-      strGlass: string;
-      strIngredient1: string;
-      strIngredient2: string;
-      strIngredient3: string;
-      strIngredient4: string;
-      strIngredient5: string;
-      strIngredient6: string;
-      strIngredient7: string;
-      strIngredient8: string;
-      strIngredient9: string;
-      strIngredient10: string;
-      strInstructions: string;
-    }[]
-  ) => {
-    let sortedCocktails: {
-      idDrink: string;
-      strDrink: string;
-      strDrinkThumb: string;
-      strGlass: string;
-      strIngredient1: string;
-      strIngredient2: string;
-      strIngredient3: string;
-      strIngredient4: string;
-      strIngredient5: string;
-      strIngredient6: string;
-      strIngredient7: string;
-      strIngredient8: string;
-      strIngredient9: string;
-      strIngredient10: string;
-      strInstructions: string;
-    }[] = [];
-    cocktails.forEach((cocktail) =>
-      sortedCocktails.push({
-        idDrink: cocktail.idDrink,
-        strDrink: cocktail.strDrink,
-        strDrinkThumb: cocktail.strDrinkThumb,
-        strGlass: cocktail.strGlass,
-        strIngredient1: cocktail.strIngredient1,
-        strIngredient2: cocktail.strIngredient2,
-        strIngredient3: cocktail.strIngredient3,
-        strIngredient4: cocktail.strIngredient4,
-        strIngredient5: cocktail.strIngredient5,
-        strIngredient6: cocktail.strIngredient6,
-        strIngredient7: cocktail.strIngredient7,
-        strIngredient8: cocktail.strIngredient8,
-        strIngredient9: cocktail.strIngredient9,
-        strIngredient10: cocktail.strIngredient10,
-        strInstructions: cocktail.strInstructions,
-      })
-    );
-    console.log(sortedCocktails);
-    sortedCocktails.sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1));
-    console.log(sortedCocktails);
-    return sortedCocktails;
-  };
-
   return (
     <div id={"cocktailCardsDisplay"}>
-      {/*  <Stack marginLeft={20} marginTop={20}> */}
-      {sortedDown ? (
-        <FaSortAlphaDown
-          onClick={toggleSort}
-          style={{
-            position: "absolute",
-            top: "70px",
-            right: "20px",
-          }}
-        />
-      ) : (
-        <FaSortAlphaUp
-          onClick={toggleSort}
-          style={{
-            position: "absolute",
-            top: "70px",
-            right: "20px",
-          }}
-        />
-      )}
-      {/* </Stack> */}
       <SimpleGrid
         columns={[1, 2, 3]}
         minChildWidth={"330px"}
@@ -137,13 +45,9 @@ export default function CocktailCardsDisplay({
       >
         {cocktails.length > 0 ? (
           <>
-            {sortedDown
-              ? cocktails.map((cocktail, index) => (
-                  <CocktailCard key={index} cocktail={cocktail} />
-                ))
-              : sorted(cocktails).map((cocktail, index) => (
-                  <CocktailCard key={index} cocktail={cocktail} />
-                ))}
+            {cocktails.map((cocktail, index) => (
+              <CocktailCard key={index} cocktail={cocktail} />
+            ))}
           </>
         ) : (
           <Text>There are no cocktails available</Text>
--- client/src/pages/AllCocktailsPage.tsx
+++ client/src/pages/AllCocktailsPage.tsx
@@ -2,6 +2,11 @@ import { Button, Center, Flex, Grid, GridItem } from "@chakra-ui/react";
 import React from "react";
 import { useEffect, useState } from "react";
 import { gql, useLazyQuery, useQuery, useReactiveVar } from "@apollo/client";
+import {
+  FaSortAlphaDown,
+  FaSortAlphaUp,
+  FaSortAmountDownAlt,
+} from "react-icons/fa";
 
 import CocktailCardsDisplay from "../components/CocktailCardsDisplay";
 import FilterButtons from "../components/FilterButtons";
@@ -10,7 +15,7 @@ import SearchBar from "../components/SearchBar";
 import { offset } from "../cache";
 import { log } from "console";
 
-const GET_DRINKS_BY_NAME_CONTAINS_ANY = gql`
+const GET_DRINKS_BY_NAME_CONTAINS = gql`
   query getDrinksByNameContainsAny($recipename: String) {
     getDrinksByNameContainsAny(recipename: $recipename) {
       idDrink
@@ -83,13 +88,112 @@ const AllCocktailsPage = () => {
   const [cocktails, setCocktails] = useState([]);
   const [visible, setVisible] = useState(true);
 
+  const [sortedDown, setSortedDown] = useState(true);
 
-  const [getQuery, { loading: loading1, error: error1, data: data1 }] = useLazyQuery(GET_DRINKS_BY_NAME_CONTAINS_ANY,{
-    variables: { recipename: query },
-    onCompleted: (data1) => {
-      setCocktails(data1.getDrinksByNameContains);
-    },
-  });
+  const toggleSort = () => {
+    setSortedDown(!sortedDown);
+    sorted(cocktails);
+  };
+
+  const sorted = (
+    cocktails: {
+      idDrink: string;
+      strDrink: string;
+      strDrinkThumb: string;
+      strGlass: string;
+      strIngredient1: string;
+      strIngredient2: string;
+      strIngredient3: string;
+      strIngredient4: string;
+      strIngredient5: string;
+      strIngredient6: string;
+      strIngredient7: string;
+      strIngredient8: string;
+      strIngredient9: string;
+      strIngredient10: string;
+      strMeasure1: string;
+      strMeasure2: string;
+      strMeasure3: string;
+      strMeasure4: string;
+      strMeasure5: string;
+      strMeasure6: string;
+      strMeasure7: string;
+      strMeasure8: string;
+      strMeasure9: string;
+      strMeasure10: string;
+      strInstructions: string;
+    }[]
+  ) => {
+    let sortedCocktails: {
+      idDrink: string;
+      strDrink: string;
+      strDrinkThumb: string;
+      strGlass: string;
+      strIngredient1: string;
+      strIngredient2: string;
+      strIngredient3: string;
+      strIngredient4: string;
+      strIngredient5: string;
+      strIngredient6: string;
+      strIngredient7: string;
+      strIngredient8: string;
+      strIngredient9: string;
+      strIngredient10: string;
+      strMeasure1: string;
+      strMeasure2: string;
+      strMeasure3: string;
+      strMeasure4: string;
+      strMeasure5: string;
+      strMeasure6: string;
+      strMeasure7: string;
+      strMeasure8: string;
+      strMeasure9: string;
+      strMeasure10: string;
+      strInstructions: string;
+    }[] = [];
+    cocktails.forEach((cocktail) =>
+      sortedCocktails.push({
+        idDrink: cocktail.idDrink,
+        strDrink: cocktail.strDrink,
+        strDrinkThumb: cocktail.strDrinkThumb,
+        strGlass: cocktail.strGlass,
+        strIngredient1: cocktail.strIngredient1,
+        strIngredient2: cocktail.strIngredient2,
+        strIngredient3: cocktail.strIngredient3,
+        strIngredient4: cocktail.strIngredient4,
+        strIngredient5: cocktail.strIngredient5,
+        strIngredient6: cocktail.strIngredient6,
+        strIngredient7: cocktail.strIngredient7,
+        strIngredient8: cocktail.strIngredient8,
+        strIngredient9: cocktail.strIngredient9,
+        strIngredient10: cocktail.strIngredient10,
+        strMeasure1: cocktail.strMeasure1,
+        strMeasure2: cocktail.strMeasure2,
+        strMeasure3: cocktail.strMeasure3,
+        strMeasure4: cocktail.strMeasure4,
+        strMeasure5: cocktail.strMeasure5,
+        strMeasure6: cocktail.strMeasure6,
+        strMeasure7: cocktail.strMeasure7,
+        strMeasure8: cocktail.strMeasure8,
+        strMeasure9: cocktail.strMeasure9,
+        strMeasure10: cocktail.strMeasure10,
+        strInstructions: cocktail.strInstructions,
+      })
+    );
+    console.log(sortedCocktails);
+    sortedCocktails.sort((a, b) => (a.strDrink > b.strDrink ? 1 : -1));
+    console.log(sortedCocktails);
+    return sortedCocktails;
+  };
+
+
+  const [getQuery, { loading: loading1, error: error1, data: data1 }] =
+    useLazyQuery(GET_DRINKS_BY_NAME_CONTAINS, {
+      variables: { recipename: query },
+      onCompleted: (data1) => {
+        setCocktails(data1.getDrinksByNameContains);
+      },
+    });
 
   useEffect(() => {
     if (data) {
@@ -132,12 +236,40 @@ const AllCocktailsPage = () => {
         <GridItem colSpan={3}>
           <SearchBar q={query} setQuery={setQuery} setAlcoholType={""} />
         </GridItem>
-          <Button onClick={() => getQuery()}>Search</Button>
-        <FilterButtons/>
+        <Button onClick={() => getQuery()}>Search</Button>
+        <GridItem>
+          {sortedDown ? (
+            <FaSortAlphaDown
+              onClick={toggleSort}
+              style={{
+                position: "absolute",
+                top: "100px",
+                right: "20px",
+              }}
+            />
+          ) : (
+            <FaSortAmountDownAlt
+              onClick={toggleSort}
+              style={{
+                position: "absolute",
+                top: "100px",
+                right: "20px",
+              }}
+            />
+          )}
+        </GridItem>
         <GridItem colSpan={4}>
           {/* <CocktailCardsDisplay cocktails={filteredCocktails} /> 
           cocktailcards need {id (som number), name, image}*/}
-          <CocktailCardsDisplay cocktails={cocktails} />
+          {cocktails.length > 0 ? (
+            sortedDown ? (
+              <CocktailCardsDisplay cocktails={cocktails} />
+            ) : (
+              <CocktailCardsDisplay cocktails={sorted(cocktails)} />
+            )
+          ) : (
+            <CocktailCardsDisplay cocktails={cocktails} />
+          )}
         </GridItem>
         {visible && (
         <Button
